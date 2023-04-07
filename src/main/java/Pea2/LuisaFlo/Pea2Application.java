47fe989c222e03f20942bf5c2784e96122529063
ANDRES <ANDRES@LAPTOP-C7HL50U1>
2023-04-06 23:09:11 -0500

    Creacion del proyecto

---
+++ src/main/java/Pea2/LuisaFlo/Pea2Application.java
@@ -0,0 +1,13 @@
+package Pea2.LuisaFlo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pea2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pea2Application.class, args);
+	}
+
+}
---
+++ src/test/java/Pea2/LuisaFlo/Pea2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Pea2.LuisaFlo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pea2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
